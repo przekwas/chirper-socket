a54ff0c2dd512cf36d9a048881c9772d1cfe1e8a
przekwas <luke@covalence.io>
2019-04-19 16:48:23 -0500

    changes

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import MainView from './views/MainView';
-import RegisterView from './views/RegisterView';
+import MagicView from './views/MagicView';
 import Navbar from './components/nav/Navbar';
 
 export interface AppProps { }
@@ -15,7 +15,7 @@ const App: React.SFC<AppProps> = () => {
             <main className="container">
                 <Switch>
                     <Route exact path='/' component={MainView} />
-                    <Route exact path='/register' component={RegisterView} />
+                    <Route exact path='/register' component={MagicView} />
                 </Switch>
             </main>
         </BrowserRouter>
